ea3abfa049ad980cf4e66cb9aff9f2806bb0f1c8
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-10-23 11:47:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.write("Hooray, we connected everything")
 
 st.write("Hello_02")
+# The Text block (previously called the HTML block in English) is a standard block used to add text, 
+# multimedia, widgets etc. to a site front page or course page. 
+# The title bar may be left blank if desired.
