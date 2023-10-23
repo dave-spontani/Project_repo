5ecadaa78d96e6233ff271a524c875d558d09a51
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-10-23 11:51:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,4 @@ import streamlit as st
 st.write("Hooray, we connected everything")
 
 st.write("Hello_02")
-#A block of text is text that's grouped, such as the paragraphs or blockquotes on a web page. 
-# Often, the text takes on the shape of a square 
-# or rectangular "block."
-# The Text block (previously called the HTML block in English) is a standard block used to add text, 
-# multimedia, widgets etc. to a site front page or course page. 
-# The title bar may be left blank if desired.
 
