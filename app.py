2ddaf46923752563a162bcf69f6b554bdca68ba0
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-10-24 15:51:06 +0200

    Implemented random function

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from Feature_01 import return_even
 from Feature_02 import return_odd
+import random
+
+
 
 original_list = [i for i in range(10)]
 
@@ -18,3 +21,4 @@ st.write(odd_list)
 
 st.write("A short demonstration of why list comprehensions are superior!")
 
+st.write(random.randint(0,20))
