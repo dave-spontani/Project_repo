7cda085f08b6b852e49b136195b25919cf089c6f
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-10-26 13:50:48 +0200

    Subheader

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import random
 import matplotlib.pyplot as plt
 import math
 
-st.set_page_config(page_title="Fondue Optimizer", page_icon=":cheese:")
+st.set_page_config(page_title="Fondue Optimizer")
 
-st.title("Welcome to the Fondue Optimizer!")
+st.title("Welcome to the Fondue Optimizer! :cheese:")
 
 st.write("First things first: Select thine cheese preferences")
 
@@ -37,7 +37,7 @@ if st.button('Get recipe'):
     total = level * number_input
     wine = total * 0.4
     maizena = total // 250
-    st.write("We will now calculate the rest of the ingredients for your ideal recipe!")
+    st.subheader("We will now calculate the rest of the ingredients for your ideal recipe!")
 
     if cheese_type == "Hardcore Swiss traditionalist":
         st.write("Moitie-moitie is an excellent fondue mix. Go buy:")
