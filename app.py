ef13a529f44affb7c6a512c435cc80f45e0edf38
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2023-10-26 13:25:14 +0200

    Changes

--- app.py
+++ app.py
@@ -7,8 +7,16 @@ import math
 
 st.set_page_config(page_title="Fondue Optimizer", page_icon=":cheese:")
 
+st.title("Welcome to the Fondue Optimizer!")
+
 number_input = st.number_input("Enter how much cheese in total you will add (in Grams)")
 
 st.write("We will now calculate the rest of the ingredients for your ideal recipe!")
 
+if number_input:
 
+    st.write("Moitie-moitie is an excellent fondue mix. Go buy:")
+    st.write(f"{number_input/2}g of Gruyère cheese (mild)")
+    st.write(f"{number_input/2}g of Vacherin fribourgeois cheese (strong)")
+
+    
