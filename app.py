3b6591f7b9115b68b204dd9d6bd0facbccc2d899
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2024-03-06 10:36:10 +0100

    Done

--- app.py
+++ app.py
@@ -4,63 +4,41 @@ from Feature_02 import return_odd
 import random
 import matplotlib.pyplot as plt
 import math
+import yagmail
 
-st.set_page_config(page_title="Fondue Optimizer")
+st.set_page_config(page_title="Rent-a-Swiss-boyfriend :cheese_wedge:")
 
-st.title("Welcome to the Fondue Optimizer! :cheese_wedge:")
+st.title("Welcome to the Rent-a-Swiss-boyfriend - your solution to finding your dream cheese-loving gentleman")
 
 st.write("First things first: Select thine cheese preferences")
 
-cheese_type = st.radio(
-    "Which cheese calls to you",
-    ["Hardcore Swiss traditionalist", "Visiting tourist", "American glutton"],
-    captions = ["Wilkomme Eidgenoss!", "Welcome to Switzerland", "Your cheese isn't actually cheese, but okay"])
+man_type = st.radio(
+    "Which Swiss dream man calls to you",
+    ["Hardcore (probably inbred) Swiss traditionalist", "International Zürich boy", "French-speaking slob"],
+    captions = ["(Dating level: Impossible if not a milkmaid)", "Our absolute best-seller", "Will insult your friends and quote pretentious french movies at you"])
+    
+
+if man_type == "Hardcore (probably inbred) Swiss traditionalist":
+    st.write("So sorry, we are out of stock of this option for now.")
+
+elif man_type == "French-speaking slob":
+    st.write("We are (luckily) not offering this option anymore")
 
 
-number_input = st.number_input("Enter how many friends will partake in your fondue (whole numbers only please):", min_value=1, max_value=15, value=5, step=1)
+elif man_type == "International Zürich boy":
+    st.write("Excellent choice. This is by far our most booked option, and at 50 USD per hour, a real catch!")
+    st.write("As this boi is in high demand, please specify the use case for which you will need to rent a swiss boyfriend. We will contact you if our agent finds this assignment pleasing")
+    user_input = st.text_area("Please describe your use case here:", "Nothing here yet...")
 
-hunger_level = st.radio("Are you and your friends hungry hungry hippos or trendy fashionistas on a diet?", 
-                        ["We have small tummies", "I would like to have room for dessert", "Put me in a coma"])
+    
+    if st.button('Submit'):
 
-
-if hunger_level == "We have small tummies":
-    level = 150
-elif hunger_level == "I would like to have room for dessert":
-    level = 200
-elif hunger_level == "Put me in a coma":
-    level = 300
-
-st.write("Check that all of your imputs are correct! Once you have verified this, click the button below")
-
-if st.button('Get recipe'):
-
-    total = level * number_input
-    wine = round(total * 0.0044, 1)
-    maizena = total // 200
-    st.subheader("We will now calculate the rest of the ingredients for your ideal recipe!", divider='red')
-
-    if cheese_type == "Hardcore Swiss traditionalist":
-        st.write("Moitie-moitie is an excellent fondue mix. Go buy:")
-        st.write(f"{int(total/2)}g of Gruyère cheese (mild)")
-        st.write(f"{int(total/2)}g of Vacherin fribourgeois cheese (strong)")
-        st.write(f"Add {wine} dl of white wine (Fondent du Valais is ideal)")
-        st.write(f"And don't forget to add {maizena} tea-spoons of Maizena")
-
-    elif cheese_type == "Visiting tourist":
-
-        st.write("We will get you started with some simple cheesy goodness:")
-        st.write(f"{int(total/2)}g of Swiss mountain cheese (mild)")
-        st.write(f"{int(total/2)}g of Gruyère cheese (mild)")
-        st.write(f"Add {wine} dl of white wine (Fondent du Valais is ideal)")
-        st.write(f"And don't forget to add {maizena} tea-spoons of Maizena")
+        server = "imap.gmail.com"
+        mail_key = "grcp zrks hihm tmyc"
+        yag = yagmail.SMTP('davimont151@gmail.com', mail_key)
+        contents = ['Booking request']
+        yag.send('david.montani@student.unisg.ch', 'A booking request has arrived', user_input)
+        yag.close()
+        
 
 
-    elif cheese_type == "American glutton":
-        st.write("I disapprove of your lifestyle choices. I just want you to know. Go get")
-        st.write(f"{int(total/2)}g of Cheddar")
-        st.write(f"{int(total/2)}g of 'Swiss' cheese (whatever that means)")
-        st.write(f"Add {wine} dl of white wine (Fondent du Valais is ideal)")
-        st.write(f"And don't forget to add {maizena} tea-spoons of Maizena")
-
-    st.write("People usually also like to add pepper and nutmeg - don't be shy in spicing things up!")
-    st.write("Enjoy!")
