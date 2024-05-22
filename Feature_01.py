6e1de09124fa91afb5070d576903ed6995c9c07c
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2024-05-22 22:46:06 +0200

    Update

--- Feature_01.py
+++ Feature_01.py
@@ -1,9 +1,11 @@
-def return_even(x):
-    return [i for i in x if i%2 ==0]
-###Text block is the primary control for 
-# displaying read-only text in apps. 
-# You can use it to display single-line or multi-line text, i
-#####Another conflict
-#Banana bread is very healthy for you
 
-#I want to add furthermore, that fondue is better than raclette!
+info_dict = {"C02_Emissions": "", "Number of Women in Leadership positions": "", "Anti-Corruption measures": ""}
+
+class CH_Report:
+
+    def __init__(self, x, y):
+        print("Create a new object of the CH_Report class")
+        self.name = x
+        self.type = y
+        self.information = info_dict
+
--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 import streamlit as st
 
 
+
+info_dict = {"C02_Emissions": "", "Number of Women in Leadership positions": "", "Anti-Corruption measures": ""}
+
+class CH_Report:
+
+    def __init__(self, x, y):
+        print("Create a new object of the CH_Report class")
+        self.name = x
+        self.type = y
+        self.information = info_dict
+
 st.title("Welcome to the Obli-Gator!")
 
 st.header("First, let us get started on some questions")
 
+name = st.text_input("What is the name of your company?")
 
+type = st.radio("What is your compliance type?",["EU", "CH", "US"])
+
+firstreport = CH_Report(name, type)
+
+if st.button("Submit"):
+    print("Test")
         
 
 
