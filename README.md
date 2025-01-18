12dd898d4b2ab4ab316b13c04fc9d546fa6a75e2
dave-spontani <66824496+dave-spontani@users.noreply.github.com>
2025-01-18 11:09:11 +0100

    Changes for new project

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Project_repo
-This is the repository for the FCS project
-
-Changed it to a fondue recipe calculator. Just felt like a better thing to do.
+Current idea I am working on basically
--- app.py
+++ app.py
@@ -3,20 +3,36 @@ import docx
 import pandas as pd 
 import io
 import plotly.express as px
+import time
 
 
-tab1, tab2, tab3 = st.tabs(["Questionnaire", "Questionnaire Part 2", "Final evaluation"])
+tab1, tab2, tab3 = st.tabs(["Budget", "Saving & Investment Goals", "Final evaluation"])
 
 with tab1:
-    st.title("Welcome to the the Mock-up Questionnaire")
+    st.title("Welcome to the Equinox project!")
+    st.write("Lets start with some questions. First off, let us define your budget goals!")
 
-    hockey_score = st.slider("How much do you enjoy hockey?", min_value=1, max_value=5)
 
-    football_score = st.slider("How much do you enjoy football?", min_value=1, max_value=5)
-    
+    st.subtitle("Lets start by setting some goals!")
+    st.write("Do you have a regular income? If yes, how high is your salary (how much lands on your bank account)?")
+    salary = st.number_input("Input your salary here")
+    st.write("Great! Next, allocate a budget to your most used categories. A suggestion list is down below. Feel free to add other categories.")
+    rent = st.number_input("Input your rent payments here")
+    groceries = st.number_input("Input your grocery payments here")
+    savings = st.number_input("How much would you like to save every month?")
+    st.write("Dave here. You get the idea. In the end we make a rough calculation and showcase the biggest savings potentials")
+
+    st.write("Excellent! Thank you for submitting your plan. If you are interested in a budget consultation with our friendly budgeting advisor 'RoboDave', feel free to press the button. You can always come back to this feature later.")
+    but = st.button("Take me to RoboDave!")
+    if but:
+        st.write("Man I was hoping you wouldnt test this out. Welp. I'm not that fast, this is only a concept demo after all, so this button leads nowhere")
+        time.sleep(2)
+        st.write("But do feel free to navigate to the second tab")
+
+
 
 with tab2:
-    st.title("Questionnaire Part 2")
+    st.title("")
     
     rugby_score = st.slider("How much do you enjoy rugby?", min_value=1, max_value=5)
 
